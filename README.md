335404b8c27b547e6390dcd5bb845b2700f44729
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-09-10 02:13:28 +0000

    fix issue #1

---
+++ README.md
@@ -0,0 +1,70 @@
+[![Build Status](https://travis-ci.org/thanhson1085/swagger-combined.svg)](https://travis-ci.org/thanhson1085/swagger-combined)
+
+[![NPM](https://nodei.co/npm/swagger-combined.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/swagger-combined/)
+
+If you are building a microservice system and using Swagger. 
+
+This packet is able to help you combine all swagger documents to only one document.
+
+For example, you have the swagger documents at http://service1/docs, http://service2/docs
+
+This will help you combine all links above to only one link http://service/docs
+
+### Installation
+```
+npm install swagger-combined
+```
+### Configuration
+Create sample configuration file:
+```
+cp -R node_modules/swagger-combined/config .
+```
+And see config/default.json as below:
+```
+{
+    "list_url": [
+        {
+            "docs": "http://petstore.swagger.io/v2/swagger.json",
+            "base_path": "http://petstore.swagger.io/",
+            "route_match": "/v2*"
+        }
+    ],
+    "info": { "title": "Example API", "version": "1.0" },
+    "port": 3000
+}
+```
+- docs: swagger document links
+- base_path: Proxy Target
+- route_match: Routes for proxy
+
+### Run
+Change config/default.json to match your swagger links
+```
+node node_modules/swagger-combined/app.js
+```
+Please make note that you changed config/default.json to match all swagger document links you have
+
+### Test
+In the default, swagger-combined run on port 3000 and included swagger-ui. So you just run http://localhost:3000 to see everything you need
+
+### License (MIT)
+Copyright (c) 2015 Nguyen Sy Thanh Son <thanhson1085@gmail.com>
+ 
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+ 
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ app.js
@@ -0,0 +1,100 @@
+var config = require('config');
+var httpProxy = require('http-proxy');
+var request = require('request');
+var q = require('q');
+var express = require('express');
+var app = express();
+
+// cross origin
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// list all swagger document urls
+var listUrl = config.get("list_url");
+
+// general infor of your application
+var info = config.get("info");
+app.get('/docs', function(req, res) {
+    getApis(listUrl).then(function(data){
+        var ret = data.reduce(function(a, i){
+            if (!a) {
+                a = i;
+            }
+            else{
+                // combines paths
+                for (key in i.paths){
+                    a.paths[key] = i.paths[key];
+                }
+                // combines definitions
+                for (key in i.definitions){
+                    a.definitions[key] = i.definitions[key];
+                }
+            }
+            return a;
+        }, false);
+        ret.info = info;
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(ret));
+    }); 
+});
+var proxy = httpProxy.createProxyServer();
+
+listUrl.forEach(function(url){
+    url.route_match.forEach(function(r){
+        // GET proxy
+        app.get(r, function(req, res){ 
+            proxy.web(req, res, { target: url.base_path });
+        });
+        // POST proxy
+        app.post(r, function(req, res){ 
+            proxy.web(req, res, { target: url.base_path });
+        });
+        // PUT proxy
+        app.put(r, function(req, res){ 
+            proxy.web(req, res, { target: url.base_path });
+        });
+        // DELETE proxy
+        app.delete(r, function(req, res){ 
+            proxy.web(req, res, { target: url.base_path });
+        });
+        // OPTIONS proxy
+        app.options(r, function(req, res){ 
+            proxy.web(req, res, { target: url.base_path });
+        });
+    });
+});
+
+
+// redirect page
+app.use('/', express.static(__dirname + '/template'));
+
+// addon swagger page
+app.use('/s', express.static(__dirname + '/node_modules/swagger-ui/dist'));
+
+// Start web server at port 3000
+var port = config.get("port");
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Combines swaggers http://%s:%s', host, port);
+});
+
+// get swagger json data from urls
+var getApis = function(urls){
+    var the_promises = [];
+    urls.forEach(function(url){
+        var def = q.defer();
+        request(url.docs, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                body = JSON.parse(body);
+                def.resolve(body)
+            }
+        });
+        the_promises.push(def.promise);
+    });
+    return q.all(the_promises);
+}
+
