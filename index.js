3711d37b0dde19cafc58669d5b129e4b6f7bd8b9
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-12-05 16:56:21 +0700

    added Authorization :hibiscus:

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var url = require('url');
 // cross origin
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
